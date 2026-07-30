4e2cc0c91b389d2b218cf6f260ef1866d68da8b3
Oreoman4ik <kononovivan59@gmai.com>
2026-07-30 17:13:13 +0300

    First Commit

---
+++ src/test/java/ru/oreoman4ik/catchup/CatchupApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.oreoman4ik.catchup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatchupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
